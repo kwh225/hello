63b6250fc45331755d1e498e1c79c518c7727c10
kwh225 <112189425+kwh225@users.noreply.github.com>
2022-09-15 22:49:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     for key, value in birthdays.items():
         # 获取距离下次生日的时间
         birth_day = get_birthday(value["birthday"], year, today)
-       def get_birthday(birthday, year, today):
+        def get_birthday(birthday, year, today):
         if birth_day == 0:
             birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
         elif year == 0:
